e61bb846e4649c3a46e61d8d87b49419778d256f
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2025-04-12 07:13:47 +0100

    added simple loading page

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen animate-spin text-7xl text-gradient">
+      ツ
+    </div>
+  )
+}
