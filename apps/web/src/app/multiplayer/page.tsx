7e11d88313f386265ab63c376f5a3b4cc7a9c189
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2026-01-02 00:09:55 +0000

    fix: prod and development enviroments separated

--- apps/web/src/app/multiplayer/page.tsx
+++ apps/web/src/app/multiplayer/page.tsx
@@ -5,6 +5,7 @@ import JoinLobbyDialog from "@/components/multiplayer/landing-page/join-lobby-di
 import { Button } from "@repo/ui/components/button"
 import { Skeleton } from "@repo/ui/components/skeleton"
 import { useQuery, useQueryClient } from "@tanstack/react-query"
+import Link from "next/link"
 import { useRouter } from "next/navigation"
 
 export default function MultiplayerPage() {
@@ -12,12 +13,16 @@ export default function MultiplayerPage() {
   const router = useRouter()
 
   const handleCheckSession = async () => {
-    const res = await fetch(process.env.NEXT_PUBLIC_API_URL! + "/check-session", {
-      method: "GET",
-      credentials: "include",
-    })
-    console.log(res)
-    return await res.json()
+    if (process.env.NEXT_PUBLIC_MULTIPLAYER_STATE === "development") {
+      const res = await fetch(process.env.NEXT_PUBLIC_API_URL! + "/check-session", {
+        method: "GET",
+        credentials: "include",
+      })
+      console.log(res)
+      return await res.json()
+    } else {
+      return { isInLobby: false }
+    }
   }
 
   const handleLeave = async () => {
@@ -45,7 +50,23 @@ export default function MultiplayerPage() {
     queryFn: handleCheckSession,
   })
 
-  if (isPending)
+  if (process.env.NEXT_PUBLIC_MULTIPLAYER_STATE === "prod")
+    return (
+      <section className="h-[calc(100vh-80px)] md:h-screen w-screen p-4 md:p-8 animate-in fade-in zoom-in duration-300">
+        <div className="flex flex-col items-center justify-center gap-6 sm:gap-4 h-full">
+          <h1 className="text-5xl sm:text-7xl text-gradient">Multiplayer</h1>
+          <p className="text-gradient text-lg sm:text-2xl text-center">
+            Currently under development! Come back later!
+          </p>
+          <div className="flex justify-between items-center gap-2 sm:mt-2">
+            <Link href="/singleplayer">
+              <Button className="w-full h-full">Soloplay</Button>
+            </Link>
+          </div>
+        </div>
+      </section>
+    )
+  else if (isPending)
     return (
       <section className="h-[calc(100vh-80px)] md:h-screen w-screen p-4 md:p-8 animate-in fade-in zoom-in duration-300">
         <div className="flex flex-col items-center justify-center gap-6 sm:gap-4 h-full">
